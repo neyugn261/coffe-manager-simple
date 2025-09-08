c6abb3832b4bff8d708b23a2f685136ec15c1ec3
ldanh270 <ducanhle.dn@gmail.com>
2025-09-08 18:23:39 +0700

    style(frontend): add initial styles for *, html, body

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
-import React from "react";
-import AppRouter from "./router";
-import "./styles/global.css";
-import "./App.css";
+import React from 'react'
+import AppRouter from './router'
+import './styles/global.css'
 
 function App() {
     return (
         <div className="App">
             <AppRouter />
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
